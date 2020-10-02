b5cce7693e8fab4ac23165e04aec8369b9ec4d68
Raj <rjkeeran@gmail.com>
2020-10-02 21:36:45 +0545

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// feather init
+feather.replace();
+
+// slick init
+  $('.banner__img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    dots: true,
+    fade: true
+  });
